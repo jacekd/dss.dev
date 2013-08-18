b9536ce683c0af9bc03d14666a3e4d65fb5f1247
Jacek Dominiak <jacek@effortable.com>
2013-08-18 13:39:24 +0200

    catchRequirements as a controller

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
-var dss = angular.module('dss', []);
+var dssApp = angular.module('dssApp', []);
 
-// Config
-// dss.config(['$httpProvider', function($httpProvider) {
-//  $httpProvider.defaults.useXDomain = true;
-//  delete $httpProvider.defaults.headers.common['X-Requested-With'];
-// }]);
 
-// Catch data
-function catchRequirements($scope, $http) {
+// catchRequirements controller
+dssApp.controller('catchRequirements', function ($scope, $http) {
   $http.defaults.headers.common.Authorization = 'Basic YWRtaW4=:YWRtaW4='; // <== base64 encoded admin:admin 
 
   $http.jsonp('http://localhost:2480/cluster/functional/Requirements?callback=DisplayFuncCtrl');
-}
+});
 
 // Get First level Requirements
 // dss.controller("RequirementsCtrl", function($scope, $http) {
@@ -22,8 +17,8 @@ function catchRequirements($scope, $http) {
 // });
 //
 
-function DisplayFuncCtrl($scope) {
-  console.log($scope);
+function DisplayFuncCtrl(data) {
+  console.log(data['result']);
 }
 
 
