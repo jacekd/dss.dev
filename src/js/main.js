4cc001d40c327d39f300fc68a059cb852ea80b36
Jacek Dominiak <jacek@effortable.com>
2013-09-03 21:04:42 +0200

    requirementThirdLevel grouping done

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-
 // Initialize Angular
 var dssApp = angular.module('dssApp', []);
 
@@ -87,6 +86,38 @@ dssApp.filter('replaceSpacesWithDash', function() {
   return function(input, scope) {
     return input.replace(/ /g,"_");
   }
+});
+
+dssApp.filter('unique', function () {
+  return function (items, filterOn) {
+    if (filterOn === false) {
+      return items;
+    }
+    if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+      var hashCheck = {}, newItems = [],
+      extractValueToCompare = function (item) {
+        if (angular.isObject(item) && angular.isString(filterOn)) {
+          return item[filterOn];
+        } else {
+          return item;
+        }
+      };
+      angular.forEach(items, function (item) {
+        var valueToCheck, isDuplicate = false;
+        for (var i = 0; i < newItems.length; i++) {
+          if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+            isDuplicate = true;
+            break;
+          }
+        }
+        if (!isDuplicate) {
+          newItems.push(item);
+        }
+      });
+      items = newItems;
+    }
+    return items;
+  };
 });
 
 // first Level Requirements controller
